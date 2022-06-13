136402e8e5c2eb89b1f33c69ca7ae589721cb52a
Ivan Goya <ivan.goya98@gmail.com>
2022-06-13 14:57:26 -0400

    commiting all the folders/files we created and worked on during class with drew. setup the file structure/template

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Comment extends Model {}
+
+Comment.init({
+    body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+    },
+    {
+        sequelize
+    }
+)
+
+module.exports = Comment;
